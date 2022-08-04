9f76ab93a0dbf24e0c0d7b92b15745088738eabd
andy.cheng <andy.cheng@uatlivibank.com>
2022-08-05 02:23:20 +0800

    init

--- app/main.py
+++ app/main.py
@@ -94,7 +94,7 @@ def main():
     chrome=get_chrome_connection()
     chrome.get("https://web.whatsapp.com/")
     time.sleep(4)
-    chrome.save_screenshot('/static/whatsapp.png')
+    print("save_screenshot="+chrome.save_screenshot('/static/whatsapp.png'))
 
     return "succes"
 
