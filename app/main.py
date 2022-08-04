ab46155b02fcba05d1ecd879659f1038dec1a2f3
andy.cheng <andy.cheng@uatlivibank.com>
2022-08-05 02:18:24 +0800

    init

--- app/main.py
+++ app/main.py
@@ -37,7 +37,9 @@ def get_chrome():
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-setuid-sandbox")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--single-process")
     chrome = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     #options.add_argument("--disable-notifications")
