3202e569ca5611648fe47a8c7b063c959ed31934
andycky <45934767+andycky@users.noreply.github.com>
2022-11-03 14:53:54 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ def get_chrome():
     options.add_argument('user-agent={0}'.format(user_agent))
     #options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")
 
-    chrome = webdriver.Remote(command_executor='http://'+config["settings"]["selenium_host"]+'/wd/hub', options=options)
+    chrome = webdriver.Remote(command_executor='http://47.242.226.29:4444//wd/hub', options=options)
     chrome.maximize_window()
     #Chrome('./chromedriver_linux', chrome_options=options)
 
